dbca2a6b9c4ab21be64e4cd8839e8609e2ee3dda
Irell Zane <itsirellzane@gmail.com>
2024-12-21 10:25:21 +0000

    Floaterm updates

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -2,7 +2,8 @@ return {
     'voldikss/vim-floaterm',
     config = function()
         vim.keymap.set('n', '<leader>ft', ':FloatermNew<CR>', { noremap = true })
-        vim.keymap.set('n', '<leader>fs', ':FloatermShow<CR>', { noremap = true })
+        vim.keymap.set('n', '<leader>fs', ':FloatermToggle<CR>', { noremap = true })
+        vim.keymap.set('t', '<C-q>', "<C-\\><C-n>:FloatermToggle<CR>", { noremap = true })
 
     end
 }
