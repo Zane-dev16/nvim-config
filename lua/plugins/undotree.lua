a480d8d2f2035bba09f7a5e7f629621fe9969318
Irell Zane <itsirellzane@gmail.com>
2025-03-10 10:32:48 +0000

    feat: persistent undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,1 +1,8 @@
-return {'mbbill/undotree'}
+return {
+  'mbbill/undotree',
+  config = function()
+    -- Enable persistent undo
+    vim.opt.undofile = true
+    vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
+  end
+}
